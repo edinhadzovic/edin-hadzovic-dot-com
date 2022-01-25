0e22179887115ca3b8f42ac262fdab457500277c
Edin Hadzovic <contact@edinhadzovic.com>
2022-01-25 15:06:41 +0100

    refactoring.

--- pages/index.js
+++ pages/index.js
@@ -12,40 +12,58 @@ export default function Home(props) {
                     </div>
                     <hr />
                     <div className="w-2/3 lg:text-xl">
-                        <p>I am full stack developer working like a <span className="text-blue-500">freelancer</span>. Love to learn, teach and write about technology, programming, science, cryptocurrency and internet culture. <a className="text-green-500 hover:text-green-700" href="/about">read more <span className="text-sm">{`>`}</span></a></p>
+                        <p>I am full stack developer working like a <span className="text-blue-500">freelancer</span>. Love to learn, teach and write about technology, programming, science, cryptocurrency and internet culture. <a className="text-green-500 hover:text-green-400" href="/about">read more <span className="text-sm">{`>`}</span></a></p>
                     </div>
                 </div>
             </div>
-            <div className="container mx-auto my-4">
+            <div className="container mx-auto space-y-12 h-screen">
                 <div>
-                    <h3 className="text-3xl">work</h3>
-                    <hr />
+                    <div>
+                        <h3 className="text-3xl">work</h3>
+                        <hr />
+                    </div>
+                    <div className="py-4 flex">
+                        <div className="w-1/2 space-y-4">
+                            <h4 className="text-2xl">Denovo Gmbh</h4>
+                            <p>Working as a full stack developer for couple months, working on B2B platforms, around recycling, and using react, nodejs, express, graphql, postgressSQL, in a Agile product development cycles.</p>
+                            <p>
+                                <a className="text-green-500 hover:text-green-400" href="">more</a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div className="p-4">
-                    <div></div>
-                    <div></div>
+                <div>
+                    <div>
+                        <h3 className="text-3xl">my projects</h3>
+                        <hr />
+                    </div>
+                    <div className="py-4 flex">
+                        <div className="w-1/2 space-y-4">
+                            <h4 className="text-2xl">Kounotori Token</h4>
+                            <p>A community driven cryptocurrency, like Doge, building community and a defi protocol using ethereum, web3, react and typescript</p>
+                            <p>
+                                <a className="text-green-500 hover:text-green-400" href="">more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3 className="text-3xl">articles</h3>
+                        <hr />
+                    </div>
+                    <div className="py-4 flex">
+                        <div className="w-1/2 space-y-4">
+                            <h4 className="text-2xl">No articles right now!</h4>
+                            <p>Working on my first article to talk about cryptocurrency in general, and if web3 is just a buzz word or a real thing.</p>
+                            <p>
+                                <a className="text-green-500 hover:text-green-400" href="">more</a>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="container mx-auto my-4">
-                <div>
-                    <h3 className="text-3xl">my projects</h3>
-                    <hr />
-                </div>
-                <div className="p-4">
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-            <div className="container mx-auto my-4">
-                <div>
-                    <h3 className="text-3xl">articles</h3>
-                    <hr />
-                </div>
-                <div className="p-4">
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
+            
         </>
     );
 };
