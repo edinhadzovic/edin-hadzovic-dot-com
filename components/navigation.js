b600067c5b56416440ed894bfe74f78e9a027c92
Edin Hadzovic <contact@edinhadzovic.com>
2022-03-23 22:40:20 +0100

    adding navigation items on navigation screen, and change post to blog.

--- components/navigation.js
+++ components/navigation.js
@@ -1,45 +1,54 @@
 import { useState } from "react";
 import { Bar, Close } from "./buttons";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export const Navigation = (props) => {
-    const [active, setActive] = useState(true);
+    const router = useRouter();
+    const [active, setActive] = useState(false);
 
     const onBarClick = () => setActive(true);
 
     return (
         <div className="flex justify-end">
-            <div className="z-20">
+            <div className={`${active ? "fixed top-4 right-15" : "" } z-20`}>
                 { !active ? <Bar onClick={onBarClick} /> : <Close onClick={() => setActive(false)} /> }
             </div>
             {active &&
-                <div className="absolute top-0 right-0 w-screen h-screen bg-gray-900 z-10 text-gray-100">
+                <div className="fixed top-0 right-0 w-screen h-screen bg-gray-900 z-10 text-gray-100">
                     <nav className="h-full">
                         <div className="grid grid-cols-1 my-16 text-center items-center text-2xl font-bold">
                             <div className="py-6 sm:py-8">
                                 <Link href="/">
-                                    <a>
+                                    <a className={router.pathname === "/" ? "line-through decoration-yellow-400" : ""}>
+                                        Home
+                                    </a>
+                                </Link>
+                            </div>
+                            <div className="py-6 sm:py-8">
+                                <Link href="/about">
+                                    <a className={router.pathname === "/about" ? "line-through decoration-yellow-400" : ""}>
                                         About me
                                     </a>
                                 </Link>
                             </div>
                             <div className="py-6 sm:py-8">
-                                <Link href="/">
-                                    <a>
+                                <Link href="/work">
+                                    <a className={router.pathname === "/work" ? "line-through decoration-yellow-400" : ""}>
                                         Work
                                     </a>
                                 </Link>
                             </div>
                             <div className="py-6 sm:py-8">
-                                <Link href="/">
-                                    <a>
+                                <Link href="/my-projects">
+                                    <a className={router.pathname === "/my-projects" ? "line-through decoration-yellow-400" : ""}>
                                         Side Projects
                                     </a>
                                 </Link>
                             </div>
                             <div className="py-6 sm:py-8">
-                                <Link href="/">
-                                    <a>
+                                <Link href="/blog">
+                                    <a className={router.pathname === "/blog" ? "line-through decoration-yellow-400" : ""}>
                                         Blog
                                     </a>
                                 </Link>
